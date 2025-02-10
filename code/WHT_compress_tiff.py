50499113a4bfc68eb8215b49688d4d6141c4cf21
williamhan <hwr9912@163.com>
2025-02-10 17:24:45 +0800

    20250210_update

--- code/WHT_compress_tiff.py
+++ code/WHT_compress_tiff.py
@@ -1,7 +1,9 @@
+# pyinstaller -F D:\ToolBox\code\WHT_compress_tiff.py
 import os
 import tifffile as tif
 import argparse
 from tqdm import tqdm
+import imagecodecs._imcd
 
 def single(image_path:str=None,
            save_dir:str=None,
@@ -26,7 +28,7 @@ def parse_args(text):
     print(f"欢迎使用WeHasTool. 我们将尝试{text}")
     parser = argparse.ArgumentParser(
         description="压缩所有目录下的tif/tiff文件的体积，默认为批处理模式, 压缩模式包含:"
-                    "1. LZW（默认）:compression='zlib'"
+                    "1. LZW（默认）:compression='LZW'"
                     "2. zlib: compression='zlib', compressionargs={'level': 8}"
                     "3. jpeg: compression='jpeg'")
     parser.add_argument("--single", "-s", action="store_true", default=False,
--- code/WHT_copy_file_by_table.py
+++ code/WHT_copy_file_by_table.py
@@ -1,4 +1,4 @@
-# pyinstaller -F D:\ToolBox\comp\WHT_copy_file_by_table.py
+# pyinstaller -F D:\ToolBox\code\WHT_copy_file_by_table.py
 import argparse
 import os
 import shutil
@@ -10,22 +10,58 @@ def single(root, table, dest, args):
     with open(table, "r") as f:
         list = f.read().split("\n")
     copy_files_set = {i for i in filter(None, list)}
-    # 遍历文件树
-    with tqdm(total=len(copy_files_set)) as pbar:
-        for crt_path, dirs, files in tqdm(os.walk(root)):
-            # 取列表交集和差集
-            exist_files = set(files) & copy_files_set
-            copy_files_set = copy_files_set - exist_files
-            for file in exist_files:
-                # 复制文件
-                shutil.copy(os.path.join(crt_path, file), dest)
-                pbar.update(1)
-            # 判断集合copy_files_set是否为空
-            if not copy_files_set:
-                return 0
-    if copy_files_set is not [] and args.result:
-        result_path = os.path.split(dest)[0]
-        with open(f"{result_path}/{args.name}.csv", "w") as f:
+    # 如果需要重命名
+    if args.rename:
+        # 如果要求复制所有重名文件
+        if args.duplicated:
+            # 遍历文件树
+            with tqdm(total=len(copy_files_set)) as pbar:
+                for crt_path, dirs, files in tqdm(os.walk(root)):
+                    # 取目录下文件名和需复制文件名交集和差集
+                    exist_files = set(files) & copy_files_set
+                    # 提取父文件夹名称
+                    father_folder_name = os.path.split(crt_path)[-1]
+                    for file in exist_files:
+                        # 复制文件
+                        shutil.copy(f"{crt_path}/{file}", f"{dest}/{father_folder_name}_{file}")
+                        pbar.update(1)
+                    # 不再判断集合copy_files_set是否为空，直接退出
+        else:
+            # 遍历文件树
+            with tqdm(total=len(copy_files_set)) as pbar:
+                for crt_path, dirs, files in tqdm(os.walk(root)):
+                    # 取目录下文件名和需复制文件名交集和差集
+                    exist_files = set(files) & copy_files_set
+                    # 更新文件名列表
+                    copy_files_set = copy_files_set - exist_files
+                    # 提取父文件夹名称
+                    father_folder_name = os.path.split(crt_path)[-1]
+                    for file in exist_files:
+                        # 复制文件
+                        shutil.copy(f"{crt_path}/{file}", f"{dest}/{father_folder_name}_{file}")
+                        pbar.update(1)
+                    # 判断集合copy_files_set是否为空
+                    if not copy_files_set:
+                        return 0
+    else:
+        # 遍历文件树
+        with tqdm(total=len(copy_files_set)) as pbar:
+            for crt_path, dirs, files in tqdm(os.walk(root)):
+                # 取目录下文件名和需复制文件名交集和差集
+                exist_files = set(files) & copy_files_set
+                copy_files_set = copy_files_set - exist_files
+                for file in exist_files:
+                    # 复制文件
+                    shutil.copy(f"{crt_path}/{file}", f"{dest}/{file}")
+                    pbar.update(1)
+                # 判断集合copy_files_set是否为空
+                if not copy_files_set:
+                    return 0
+    # 如果集合非空且要求输出总结文件
+    if copy_files_set is not [] and args.summary and not args.duplicated:
+        summary_path = os.path.split(dest)[0]
+        print(f"一共有{len(copy_files_set)}个文件未找到！")
+        with open(f"{summary_path}/{args.name}.csv", "w") as f:
             f.write("\n".join(copy_files_set)+"\n")
 
 
@@ -39,11 +75,18 @@ def parse_args(text):
     print(f"欢迎使用WeHasTool. 我们将尝试{text}")
     parser = argparse.ArgumentParser(
         description="按给定列表给出的文件名在指定目录下寻找文件，并将匹配到的文件复制到指定路径")
-    parser.add_argument("--result", "-r", action="store_true", default=False,
+    parser.add_argument("--summary", "-s", action="store_true", default=False,
                         help="是否输出结果文件名列表（包含所有未匹配到的文件名，文件名以回车符分隔），默认为否")
-    parser.add_argument("--name", "-n", type=str, default="result",
-                        help="输出结果列表的csv文件名（不含后缀），默认为result")
+    parser.add_argument("--name", "-n", type=str, default="summary",
+                        help="输出结果列表的csv文件名（不含后缀），默认为summary")
+    parser.add_argument("--rename", "-r", action="store_true", default=False,
+                        help="是否将文件名改成 上级文件夹_原名 的格式，默认为否")
+    parser.add_argument("--duplicated", "-d", action="store_true", default=False,
+                        help="是否允许同一文件名在不同路径下多次搜索，默认为否(危险操作，谨慎开启，开启后--rename设定为True)")
     args = parser.parse_args()
+
+    if args.duplicated:
+        args.rename = True
 
     return args
 
